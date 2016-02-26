efea98cc0b771bc0e2becfd2fe915cbf1ddcd707
Billiam <hello@billiam.io>
2016-02-26 11:23:15 -0800

    in debug page: basic modal and layouts done

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  // Modal stuff, can probably refactor
+  $( ".modal-button" ).click(function() {
+    $( ".modal" ).toggleClass( "is-active" );
+  });
+  $( ".modal-background" ).click(function() {
+    $( ".modal" ).toggleClass( "is-active" );
+  });
+  $( ".modal-close" ).click(function() {
+    $( ".modal" ).toggleClass( "is-active" );
+  });
 });
